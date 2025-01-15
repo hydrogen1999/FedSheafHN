686f8d15a50d81a05af1bbe84990dc2b41c608b4
hydrogen1999 <nguyencongthinh1999@gmail.com>
2025-01-15 23:23:28 +0700

    call SheafDiffusionAggregator

--- aggregator/factory.py
+++ aggregator/factory.py
@@ -1,13 +1,11 @@
 # aggregator/factory.py
 
+from aggregator.base import AggregatorBase
 from aggregator.sheaf import SheafDiffusionAggregator
-# from aggregator.hodge import HodgeAggregator  # v.v.
 
 def get_aggregator(args):
-    agg_type = args.get('aggregator', 'sheaf')
-    if agg_type == 'sheaf':
+    agg_type = args.get('aggregator','sheaf')
+    if agg_type=='sheaf':
         return SheafDiffusionAggregator(args)
-    # elif agg_type == 'hodge':
-    #    return HodgeAggregator(args)
     else:
-        raise ValueError(f"Unknown aggregator: {agg_type}")
+        raise ValueError(f'Unknown aggregator: {agg_type}')
